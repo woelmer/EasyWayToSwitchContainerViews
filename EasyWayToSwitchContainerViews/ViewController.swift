7a72befc6d956d6e2d0eb6ff6bc54dd85bd7dcbb
Mike Woelmer <mike.woelmer@atomicobject.com>
2016-06-09 10:54:28 -0400

    Initial Commit

---
+++ EasyWayToSwitchContainerViews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EasyWayToSwitchContainerViews
+//
+//  Created by Mike Woelmer on 6/9/16.
+//  Copyright © 2016 atomicobject. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
